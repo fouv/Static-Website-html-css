ec6da10bb9f67e2ffc8bef2241c448cbb12cc62c
Florence Bouvier <florencebres1@gmail.com>
2017-03-02 18:20:45 +0100

    add who are you ?

--- page/home.php
+++ page/home.php
@@ -41,7 +41,31 @@
                 <div class="container">
                     <h1 >Qui sommes-nous ?</h1>
 
-                                    </div>
+             <img src="images/julien.png" class="img-circle" alt="Cinque Terre" width="128" height="128">
+
+                <p class="text-justify">lusieurs variations de Lorem Ipsum peuvent
+                être trouvées ici ou là, mais la majeure partie d'entre elles a été altérée par l'addition
+                d'humour ou de mots aléatoires qui ne ressemblent pas une seconde à du texte standard.
+                Si vous voulez utiliser un passage du Lorem Ipsum, vous devez être sûr qu'il n'y a rien
+                d'embarrassant caché dans le texte. Tous les générateurs de Lorem Ipsum sur Internet
+                tendent à reproduire le même extrait sans fin, ce qui fait de lipsum.com
+                le seul vrai générateur de Lorem Ipsum. Iil utilise un dictionnaire de plus de
+                200 mots latins.</p>
+             <img src="images/damien-armente-profil.jpg" class="img-circle" alt="Cinque Terre" width="128" height="128">_
+
+                <p class="text-justify">lusieurs variations de Lorem Ipsum peuvent
+                    être trouvées ici ou là, mais la majeure partie d'entre elles a été altérée par l'addition
+                    d'humour ou de mots aléatoires qui ne ressemblent pas une seconde à du texte standard.
+                    Si vous voulez utiliser un passage du Lorem Ipsum, vous devez être sûr qu'il n'y a rien
+                    d'embarrassant caché dans le texte. Tous les générateurs de Lorem Ipsum sur Internet
+                    tendent à reproduire le même extrait sans fin, ce qui fait de lipsum.com
+                    le seul vrai générateur de Lorem Ipsum. Iil utilise un dictionnaire de plus de
+                    200 mots latins.</p>
+        </div>
+
+
+
+
             </div>
         </div>
     </div>
