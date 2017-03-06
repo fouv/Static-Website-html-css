0d33f9b43db4e772cf0c4f890d687e029ca4e4aa
Florence Bouvier <florencebres1@gmail.com>
2017-03-06 15:15:43 +0100

    new change

--- inc/header.php
+++ inc/header.php
@@ -1,23 +1,26 @@
 <!--Navbar -->
 <div class = "row">
-<nav class="navbar navbar-inverse">
-    <div class="navbar-header">
+ <nav class="navbar navbar-default navbar-static-top sticky-header" >
+     <div class="full-wrapper">
+
+    <div class="navbar-header" >
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
+        <div class="navbar-brand" ><img src="images/logo_restaurant.jpg"></div>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-            <li >LOGO</li>
+        <ul class="nav navbar-nav navbar-right" >
             <li ><a href="#anchor_concept">Concept</a></li>
-            <li ><a href="#anchor_restaurants">Restaurants</a></li>
-            <li ><a href="">Devenir franchisé</a></li>
+            <li ><a href="#anchor_restaurants">Nos restaurants</a></li>
+            <li ><a href="">Réservation</a></li>
+            <li ><a href="" class="btn btn-primary">Devenir franchisé</a></li>
             <li ><a href="">Contact</a></li>
-
         </ul>
     </div>
+ </div>
 </nav>
-</div>
+</nav>
 <!--end of navbar-->
--- page/home.php
+++ page/home.php
@@ -6,7 +6,6 @@
             <li data-target="#MYCAROUSEL" data-slide-to="2"></li>
             <li data-target="#MYCAROUSEL" data-slide-to="3"></li>
         </ol>
-
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
@@ -34,44 +33,34 @@
         </a>
     </div>
     </div> <!--End carousel-->
+    <!-- Home -->
 
+      <div class="home">
+            <div class="row">
+                <div class="col-xs-3 col-md-4 col-lg-3 " >
+                    <h3>Le succès d’une cuisine traditionnellement cuisinée devant vous</h3>
+                   <p>Depuis 1934, Steak ’n Shake propose à ses clients des burgers premium de qualité, des frites fraîches « maison » ainsi que des
+                    Milkshakes traditionnels primés n°1 par le guide gastronomique ZAGAT aux Etats- Unis.
+                   Notre concept de cuisine ouverte et visible par les clients fait partie intégrante de notre philosophie et de notre design.
+                       Ce concept représente un facteur clé de notre succès car il met en exergue la qualité de notre savoir-faire et la fraîcheur de nos produits.</p>
+                <img src="../images/plat.jpg" class="src-col">
+                </div>
+                <div class="col-xs-4 col-md-4 col-lg-4 " >
+                    <h3 >La meilleure qualité, au meilleur prix</h3>
+                  <p>Nous défendons des valeurs qui nous sont essentielles, à savoir d’offrir à nos consommateurs une qualité supérieure en termes de produits et de service,
+                    et cela au meilleur prix.Steak ’n Shake a su combiner son savoir-faire traditionnel aux produits de haute qualité française et européenne,
+                      tout en conservant des prix extrêmement concurrentiels, créant ainsi un positionnement unique en France, et plus généralement en Europe.</p>
 
-    <!-- Home concept -->
-    <div class="container">
-      <div class="codrops-top clearfix">
-      </div>
-      <div>
+                    <img src="../images/plat.jpg" class="src-col">
+                </div>
+                <div class="col-xs-4 col-md-4 col-lg-4 " >
+                    <h3>Des endroits inoubliables</h3>
+                    <p>Qui n'aimerait pas profiter d'un merveilleux décor pour se relaxer en profitant d'un bon repas ? Etre au bord de la mer, au coeur d'une forêt
+                        ou même  suspendu dans les airs... tant de lieux qui font rêver et qui existent réellement. C'est ce que nous vous offrons sur un plateau !</p>
 
-          <h1 id="anchor_concept">Concept</h1>
-          <H2 class="text-center">RESTAURANTS ATYPIQUES : UN POINT DE VUE UNIQUE <br>SUR LA RESTAURATION</H2>
-          Le concept de franchise de restaurants atypiques a été créé par Julien Boyer et Damien Armenté, deux passionnés de gastronomie hors pair.
-             Le premièr restaurant "montgolfière" est ainsi née à Lyon en 2015. Le succès étant au rendez-vous, six autres restaurants ont vu le jour en moins d'un an : la Loupe, Fontainebleau, Orléans,
-              Bordeaux, Toulouse et Marseille...
-   </div><!-- /home concept -->
-    <!--Presentation -->
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h1 id="anchor_who">Qui sommes-nous ?</h1>
-        </div>
-         <div class="row">
-             <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                 <h2> Damien Armenté </h2>
-             </div>
-             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                 <img src="images/damien_armente.jpg" class="img-circle" alt="Damien Armenté" width="128" height="128">_
-             </div>
-         </div>
-        <div class="row">
-            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-               <h2> Julien Boyen </h2>
+                    <img src="../images/plat.jpg" class="src-col">
+                </div>
             </div>
-            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <img src="images/julien.png" class="img-circle" alt="Julien Boyer" width="128" height="128">
-            </div>
-        </div>
 
-
-
-
-    <!-- end of presentation -->
+                <!-- end of presentation -->
     <title>Créer une carte API Google Maps Version 3</title>
