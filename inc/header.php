cb7a62f09e88fe844db626e36d63f56bdc15b630
Johan Gaub <johangaub@gmail.com>
2017-03-07 18:19:31 +0100

    add mentions+less css

--- inc/header.php
+++ inc/header.php
@@ -1,26 +1,27 @@
 <!--Navbar -->
-<div class = "row">
- <nav class="navbar navbar-default navbar-static-top sticky-header" >
-     <div class="full-wrapper">
+<div id="anchor_menu" class="row">
+    <nav class="navbar navbar-default navbar-static-top sticky-header" >
+        <div class="full-wrapper">
 
-    <div class="navbar-header" >
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <div class="navbar-brand" ><img src="images/logo_restaurant.jpg"></div>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav navbar-right" >
-            <li ><a href="#anchor_concept">Concept</a></li>
-            <li ><a href="#anchor_restaurants">Nos restaurants</a></li>
-            <li ><a href="">Réservation</a></li>
-            <li ><a href="" class="btn btn-primary">Devenir franchisé</a></li>
-            <li ><a href="">Contact</a></li>
-        </ul>
-    </div>
- </div>
-</nav>
-</nav>
+            <div class="navbar-header" >
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="navbar-brand" ><img src="images/logo_restaurant.jpg"></div>
+            </div>
+
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right" >
+                    <li ><a href="#anchor_concept">Concept</a></li>
+                    <li ><a href="#anchor_restaurants">Nos restaurants</a></li>
+                    <li ><a href="">Réservation</a></li>
+                    <li ><a href="" class="btn btn-primary">Devenir franchisé</a></li>
+                    <li ><a href="#anchor_contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
 <!--end of navbar-->
--- page/contact.php
+++ page/contact.php
@@ -4,67 +4,56 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     var_dump($_POST);
     print_r('</pre>');
 } else {
-?>
+    ?>
 
 
+    <div class="container">
+        <div class="row">
+            <h2 id="anchor_contact" class="title2">Contact</h2>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <form class="form-horizontal FORM" action="" method="post" role="form">
 
 
-
-    <!--<div class="row">
-        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"></div>
-
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">-->
-
-
-<div class="container">
-    <div class="row">
-        <h2 id="anchor_contact">Contact</h2>
-        <div class="col-md-6">
-
-            <form class="form-inline" action="" method="post" role="form">
-
-                <label for="exampleInputCivility">Civilité</label>
-                    <select class="form-control saut" id="exampleInputCivility">
-                        <option>Madame</option>
-                        <option>Mademoiselle</option>
-                        <option>Monsieur</option>
-                        <option>Transgenre</option>
-                    </select>
-
-                <div class="form-group">
-                    <label for="exampleInputName1">Prénom</label>
-                    <input type="text" class="form-control saut" id="exampleInputName1" placeholder="David">
-                </div>
-
-                <div class="form-group">
-                    <label for="exampleInputName2">Nom</label>
-                    <input type="text" class="form-control saut" id="exampleInputName2" placeholder="Hasseloff">
-                </div>
-
-                 <div class="form-group">
-                    <label for="exampleInputEmail2" >Email</label>
-                    <input type="email" class="form-control saut" id="exampleInputEmail2" placeholder="davidhasselhoff@awesome.com">
-                </div>
-
-                <div class="form-group">
-                    <label for="exampleInputTelNumber">Téléphone</label>
-                    <input type="tel" class="form-control saut" id="exampleInputTelNumber" placeholder="0677002000">
-                </div>
-
-
-                 <div class="form-group">
-                    <label for="exampleInputTown">Dans quelle ville souhaitez-vous devenir franchisé Wild Restaurant ?</label>
-                    <input type="tel" class="form-control saut" id="exampleInputTown" placeholder="Nice / Saint Jean de Luz / Belle ile ?...">
-                </div>
-
-            </form>
-        </div>
-
-                <div class="col-md-6">
                     <div class="form-group">
-                        <label for="comment">Exprimez-vous</label>
-                        <textarea class="form-control saut" id="comment" rows="10" placeholder="Je souhaite créer une franchise de Wild Restaurant dans le ville de _____ avec le concept de ____ car ..."></textarea>
+                        <label for="exampleInputCivility">Civilité</label>
+                        <select class="form-control saut" id="exampleInputCivility">
+                            <option>Madame</option>
+                            <option>Mademoiselle</option>
+                            <option>Monsieur</option>
+                        </select>
                     </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputName1">Prénom</label>
+                        <input type="text" class="form-control saut" id="exampleInputName1" placeholder="David">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputName2">Nom</label>
+                        <input type="text" class="form-control saut" id="exampleInputName2" placeholder="Hasseloff">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail2" >Email</label>
+                        <input type="email" class="form-control saut" id="exampleInputEmail2" placeholder="davidhasselhoff@awesome.com">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputTelNumber">Téléphone</label>
+                        <input type="tel" class="form-control saut" id="exampleInputTelNumber" placeholder="0677002000">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputTown">Dans quelle ville souhaitez-vous devenir franchisé Wild Restaurant ?</label>
+                        <input type="tel" class="form-control saut" id="exampleInputTown" placeholder="Nice / Saint Jean de Luz / Belle ile ?...">
+                    </div>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <div class="form-group">
+                    <label for="comment">Exprimez-vous</label>
+                    <textarea class="form-control saut" id="comment" rows="10" placeholder="Je souhaite créer une franchise de Wild Restaurant dans le ville de _____ avec le concept de ____ car ..."></textarea>
                 </div>
 
                 <div class="form-group">
@@ -73,13 +62,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <p class="help-block">Cela peut etre un dossier de projet ou autre business plan.</p>
                 </div>
 
-                 <button type="submit" class="btn btn-primary">Envoyer</button>
+                <button type="submit" class="btn btn-primary">Envoyer</button>
 
+            </div>
+            </form>
 
-
+        </div>
     </div>
 
-    </div>
 
 
 <?php } ?>
