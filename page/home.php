915f5df62703519c4484d447dac1c2207dc7dd3c
Florence Bouvier <florencebres1@gmail.com>
2017-03-08 15:37:43 +0100

    add new style css and home

--- page/home.php
+++ page/home.php
@@ -40,7 +40,7 @@
               <div class="title2">CONCEPT</div>
             <div class="row text-center">
                 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 " >
-                    <div class="title3">Le succès d’une cuisine traditionnellement cuisinée devant vous</div>
+                    <div class="title3">Le succès d’une cuisine traditionnellement cuisinée devant vous </div>
                    <div class="paragraphe">Nous proposons à nos clients des produits de qualité, faits « maison » ainsi que des
                     menus traditionnels primés n°1 par le guide gastronomique "Michelin".
                    Notre concept de cuisine ouverte et visible par les clients fait partie intégrante de notre philosophie et de notre design.
@@ -49,41 +49,18 @@
                 </div>
                 <div class="col-xs-12  col-sm-6 col-md-4 col-lg-4 " >
                     <div class="title3">La meilleure qualité, au meilleur prix</div>
+                    <img src="../images/culinaire.jpg"  class="img-responsive">
                   <div class="paragraphe">Nous défendons des valeurs qui nous sont essentielles, à savoir d’offrir à nos consommateurs une qualité supérieure en termes de produits et de service,
                     et cela au meilleur prix.Nos restaurans ont su combiner leur savoir-faire traditionnel aux produits de haute qualité française et européenne,
                       tout en conservant des prix extrêmement concurrentiels, créant ainsi un positionnement unique en France.</div>
-<div class="home">
-    <div class="row">
-        <div class="col-xs-3 col-md-4 col-lg-3 " >
-            <div class="title3">Le succès d’une cuisine traditionnellement cuisinée devant vous</div>
-            <div class="paragraphe">Depuis 1934, Steak ’n Shake propose à ses clients des burgers premium de qualité, des frites fraîches « maison » ainsi que des
-                Milkshakes traditionnels primés n°1 par le guide gastronomique ZAGAT aux Etats- Unis.
-                Notre concept de cuisine ouverte et visible par les clients fait partie intégrante de notre philosophie et de notre design.
-                Ce concept représente un facteur clé de notre succès car il met en exergue la qualité de notre savoir-faire et la fraîcheur de nos produits.</div>
-            <img src="../images/plat.jpg" class="src-col">
-        </div>
-        <div class="col-xs-4 col-md-4 col-lg-4 " >
-            <div class="title3">La meilleure qualité, au meilleur prix</div>
-            <div class="paragraphe">Nous défendons des valeurs qui nous sont essentielles, à savoir d’offrir à nos consommateurs une qualité supérieure en termes de produits et de service,
-                et cela au meilleur prix.Steak ’n Shake a su combiner son savoir-faire traditionnel aux produits de haute qualité française et européenne,
-                tout en conservant des prix extrêmement concurrentiels, créant ainsi un positionnement unique en France, et plus généralement en Europe.</div>
-
-                    <img src="../images/plat.jpg"  class="img-responsive">
-                </div>
+                                  </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 " >
-                    <div>Des endroits inoubliables</div>
+                    <div class="title3">Des endroits inoubliables</div>
                     <div class="paragraphe">Qui n'aimerait pas profiter d'un merveilleux décor pour se relaxer en profitant d'un bon repas ? Etre au bord de la mer, au coeur d'une forêt
                         ou même  suspendu dans les airs... tant de lieux qui font rêver et qui existent réellement. C'est ce que nous vous offrons sur un plateau !</div>
-            <img src="../images/plat.jpg" class="src-col">
+            <img src="../images/restaurant-christian.jpg" class="src-col">
         </div>
-        <div class="col-xs-4 col-md-4 col-lg-4 " >
-            <div class="title3">Des endroits inoubliables</div>
-            <div class="paragraphe">Qui n'aimerait pas profiter d'un merveilleux décor pour se relaxer en profitant d'un bon repas ? Etre au bord de la mer, au coeur d'une forêt
-                ou même  suspendu dans les airs... tant de lieux qui font rêver et qui existent réellement. C'est ce que nous vous offrons sur un plateau !</div>
-
-                    <img src="../images/plat.jpg"  class="img-responsive">
-                </div>
-            </div>
+                    </div>
             </div>
       </div>
 
@@ -91,7 +68,7 @@
 
           <div class="container" >
               <div class="default-heading">
-                  <h2>NOS RESTAURANTS</h2>
+                  <div class="title2">NOS RESTAURANTS</div>
               <div class="row" class="img-responsive">
                   <section class="home">
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
