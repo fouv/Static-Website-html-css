5d64fcb5aa064d2ee5b97bf3a179bc76b3063c6c
Johan Gaub <johangaub@gmail.com>
2017-03-10 09:20:32 +0100

    padding title 2 corrected

--- page/restaurant.php
+++ page/restaurant.php
@@ -31,8 +31,6 @@
                 </div>
             </div>
         </div>
-
-
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <div class="restaurant">
                 <div class="squarebg">
@@ -61,8 +59,6 @@
         </div>
     </div>
 </div>
-
-
 <div class="container">
     <div class="title2"> ou nous trouver ?</div>
     <div class="row">
@@ -72,133 +68,133 @@
                     <div class="panel panel-default">
                         <div class="panel panel-primary">
                             <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseOne">
-                                WILD LYON
-                            </a><i class="indicator glyphicon glyphicon-chevron-down  pull-right"></i>
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseOne">
+                                    WILD LYON
+                                </a><i class="indicator glyphicon glyphicon-chevron-down  pull-right"></i>
+                            </div>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                104 AVENUE DES LOGISSONS <br>
+                                69002 LYON<br>
+                                Tél (33) 04 42 38 25 08<br>
+                            </div>
                         </div>
                     </div>
-                    <div id="collapseOne" class="panel-collapse collapse in">
-                        <div class="panel-body">
-                            104 AVENUE DES LOGISSONS <br>
-                            69002 LYON<br>
-                            Tél (33) 04 42 38 25 08<br>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="title4">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseTwo">
+                                    LA LOUPE
+                                </a><i class="indicator glyphicon glyphicon-chevron-up  pull-right"></i>
+                            </div>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                18 Rue de la gare<br>
+                                28240 La Loupe<br>
+                                +06 19 34 42 37<br>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseTwo">
-                                LA LOUPE
-                            </a><i class="indicator glyphicon glyphicon-chevron-up  pull-right"></i>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="title4">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseThree">
+                                    FONTAINEBLEAU
+                                </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                            </div>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Les Mines Paris Tech<br>
+                                35 rue Saint Honoré <br>
+                                77300 Fontainebleau <br>
+                                +33 6 58 46 83 32
+                            </div>
                         </div>
                     </div>
-                    <div id="collapseTwo" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            18 Rue de la gare<br>
-                            28240 La Loupe<br>
-                            +06 19 34 42 37<br>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="title4">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseOne">
+                                    ORLEANS
+                                </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                            </div>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Le Lab’O <br>
+                                1 Avenue du champ de Mars <br>
+                                45100 Orléans <br>
+                                +33 6 62 39 71 04
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseThree">
-                                FONTAINEBLEAU
-                            </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="title4">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseTwo">
+                                    BORDEAUX
+                                </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                            </div>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                15 Place Canteloup <br>
+                                33800 Bordeaux <br>
+                                +33 6 66 96 17 77
+                            </div>
                         </div>
                     </div>
-                    <div id="collapseThree" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            Les Mines Paris Tech<br>
-                            35 rue Saint Honoré <br>
-                            77300 Fontainebleau <br>
-                            +33 6 58 46 83 32
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="title4">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseThree">
+                                    TOULOUSE
+                                </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                            </div>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                1 Place de la bourse <br>
+                                31000 Toulouse <br>
+                                +33 6 66 47 68 70
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseOne">
-                                ORLEANS
-                            </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="title4">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
+                                   href="#collapseThree">
+                                    MARSEILLE
+                                </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
+                            </div>
                         </div>
-                    </div>
-                    <div id="collapseOne" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            Le Lab’O <br>
-                            1 Avenue du champ de Mars <br>
-                            45100 Orléans <br>
-                            +33 6 62 39 71 04
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseTwo">
-                                BORDEAUX
-                            </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
-                        </div>
-                    </div>
-                    <div id="collapseTwo" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            15 Place Canteloup <br>
-                            33800 Bordeaux <br>
-                            +33 6 66 96 17 77
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseThree">
-                                TOULOUSE
-                            </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
-                        </div>
-                    </div>
-                    <div id="collapseThree" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            1 Place de la bourse <br>
-                            31000 Toulouse <br>
-                            +33 6 66 47 68 70
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="title4">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                               href="#collapseThree">
-                                MARSEILLE
-                            </a><i class="indicator glyphicon glyphicon-chevron-up pull-right"></i>
-                        </div>
-                    </div>
-                    <div id="collapseThree" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            70 rue Morellon <br>
-                            13000 Marseille <br>
-                            +33 6 32 26 40 32
+                        <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                70 rue Morellon <br>
+                                13000 Marseille <br>
+                                +33 6 32 26 40 32
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <div>
-                <div id="map"></div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <div>
+                    <div id="map"></div>
+                </div>
             </div>
         </div>
+        <div>
+            <a class="js-scrollTo anchor" href="#anchor_menu">^</a>
+        </div>
     </div>
-    <div>
-        <a class="js-scrollTo anchor" href="#anchor_menu">^</a>
-    </div>
-</div>
